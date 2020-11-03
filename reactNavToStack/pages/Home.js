6f46a5cc13c9256f5231e2dc0c33c0141ab507c5
CindoddCindy <cindodcindy@gmail.com>
2020-11-03 14:42:15 +0700

    cuma tmbah button

--- reactNavToStack/pages/Home.js
+++ reactNavToStack/pages/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-import { StyleSheet, View, Platform } from 'react-native';
+import { StyleSheet, View, Platform,Text ,SafeAreaView,Button} from 'react-native';
 
 import Slideshow from 'react-native-slideshow';
 
 
 export default class Home extends React.Component
 {
+  
 
   constructor() {
 
@@ -17,16 +18,11 @@ export default class Home extends React.Component
       interval: null,
       dataSource: [
         {
-          title: 'Title 1',
-          caption: 'Caption 1',
+         
           url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTMDAYYy6_dlRvoJvb6zl-9xMGJWos1GZitbQ&usqp=CAU',
         }, {
-          title: 'Title 2',
-          caption: 'Caption 2',
           url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ03zFSgiErEM3_8wTKM0CKlxcTx5W6cynHfA&usqp=CAU',
         }, {
-          title: 'Title 3',
-          caption: 'Caption 3',
           url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTKJYUlNRtK2OoYOg3NTVgio8QwuVxOLdcImQ&usqp=CAU',
         },
       ],
@@ -47,20 +43,40 @@ export default class Home extends React.Component
   componentWillUnmount() {
     clearInterval(this.state.interval);
   }
+    
 
   render() {
 
     return (
 
       <View style={styles.MainContainer}>
-
+        
         <Slideshow
           dataSource={this.state.dataSource}
           position={this.state.position}
           onPositionChanged={position => this.setState({ position })}
            />
+             
 
+            
+             <SafeAreaView style={{ flex: 1 }}>
+      {/* enclose all components in this View tag */}
+      <View style={styles.container}>
+        
+        {/* Button whith handler function named onPressLearnMore*/}
+        <Button
+         
+         title="UserName"
+         color="#FFF8E1"
+         
+        />
+
+        
       </View>
+    </SafeAreaView>
+             </View>
+          
+    
     );
   }
 }
@@ -73,6 +89,35 @@ const styles = StyleSheet.create({
     paddingTop: (Platform.OS) === 'ios' ? 20 : 0,
     backgroundColor: '#FFF8E1'
 
+  },
+  
+  NameStyle:{
+      flex:1,
+      justifyContent:'space-between',
+      backgroundColor : '#670099',
+      width: 500,
+    height: 50,
+      
+      
+
+  },
+  flexStyle:{
+    width: 500,
+    height: 50, 
+    backgroundColor: 'purple'
+
+  },
+  TextsStyle : {
+    color: '#670099',
+    textAlign: 'left',
+    fontWeight: 'bold',
+    fontSize: 14,
+
+  },
+  container: {
+    flex: 1,
+    width: 500
+   
   }
 
 });
